11e2d89f44857002a513aa4417143ce298ede4f0
Sham-Nab1 <shamfrancisco06@gmail.com>
2024-10-07 20:20:03 +0800

    doc: adding Restaurant_list.py

---
+++ restaurant_list.py
@@ -0,0 +1,11 @@
+# Restaurant Table
+restaurant_list = [
+    [1, "Vikings Luxury Buffet", "Pasay City", "Buffet", 2011, "www.vikings.ph"],
+    [2, "Antonio's Restaurant", "Tagaytay", "Fine Dining", 2002, "www.antoniosrestaurant.ph"],
+    [3, "Mesa Filipino Moderne", "Makati City", "Filipino", 2009, "www.mesa.ph"],
+    [4, "Manam Comfort Filipino", "Quezon City", "Filipino", 2013, "www.manam.ph"],
+    [5, "Ramen Nagi", "Various Locations", "Japanese", 2013, "www.ramennagi.com.ph"]
+]
+
+# Print the restaurant list
+print(restaurant_list)
