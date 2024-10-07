8f10fc1f0f71571b19c89af48291f101983cdecd
Sham-Nab1 <shamfrancisco06@gmail.com>
2024-10-07 20:18:11 +0800

    doc: adding Books_list.py

---
+++ books_list.py
@@ -0,0 +1,11 @@
+# Books Table
+books_list = [
+    [1, "The Great Gatsby", "F. Scott Fitzgerald", "Fiction", 1925, "978-0743273565", 20, 15.99],
+    [2, "To Kill a Mockingbird", "Harper Lee", "Fiction", 1960, "978-0060935467", 35, 10.99],
+    [3, "1984", "George Orwell", "Dystopian", 1949, "978-0451524935", 40, 9.99],
+    [4, "The Catcher in the Rye", "J.D. Salinger", "Fiction", 1951, "978-0316769488", 25, 8.99],
+    [5, "A Brief History of Time", "Stephen Hawking", "Non-fiction", 1988, "978-0553380163", 10, 18.99]
+]
+
+# Print the books list
+print(books_list)
