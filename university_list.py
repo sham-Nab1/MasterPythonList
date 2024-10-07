ce8632360b97851896a37bce71f83bd027d54f9d
Sham-Nab1 <shamfrancisco06@gmail.com>
2024-10-07 20:19:15 +0800

    doc: adding University_list.py

---
+++ university_list.py
@@ -0,0 +1,11 @@
+# University Table
+university_list = [
+    [1, "University of the Philippines", "Quezon City", 1908, "Public", "www.up.edu.ph"],
+    [2, "Ateneo de Manila University", "Quezon City", 1859, "Private", "www.ateneo.edu"],
+    [3, "De La Salle University", "Manila", 1911, "Private", "www.dlsu.edu.ph"],
+    [4, "University of Santo Tomas", "Manila", 1611, "Private", "www.ust.edu.ph"],
+    [5, "Polytechnic University of the Philippines", "Manila", 1904, "Public", "www.pup.edu.ph"]
+]
+
+# Print the university list
+print(university_list)
